ed217494283d1b5a2c050bb21726788ff2968b48
Victor Quach <varaal@gmail.com>
2017-01-31 15:13:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Chrome extension for Lyrics.ovh
---
+++ popup.js
@@ -0,0 +1,84 @@
+/**
+ * Author: Varal7
+ * Date: Jan 2017
+ **/
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabTitle(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    var tab = tabs[0];
+    var title = tab.title;
+
+    console.log(title);
+    console.assert(typeof title == 'string', 'tab.url should be a string');
+    callback(title);
+  });
+}
+
+/**
+ * @param {string} searchTerm - Search term for Lyrics.ovh
+ * @param {function(string,number,number)} callback - Called when lyrics have
+ *   been found.
+ * @param {function(string)} errorCallback - Called when the image is not found.
+ *   The callback gets a string that describes the failure reason.
+ */
+function getLyrics(artist, songTitle, callback, errorCallback) {
+  // Google image search - 100 searches per day.
+  // https://developers.google.com/image-search/
+  var searchUrl = 'https://api.lyrics.ovh/v1/' + artist + '/' + songTitle;
+  var x = new XMLHttpRequest();
+  x.open('GET', searchUrl);
+  // The Google image search API responds with JSON, so let Chrome parse it.
+  x.responseType = 'json';
+  x.onload = function() {
+    // Parse and process the response from Google Image Search.
+    var response = x.response;
+    if (!response || !response.lyrics ||
+        response.lyrics.length === 0) {
+      errorCallback('No response from lyrics.ovh API!');
+      return;
+    }
+    callback(response.lyrics);
+  };
+  x.onerror = function() {
+    errorCallback('Network error.');
+  };
+  x.send();
+}
+
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  getCurrentTabTitle(function(title) {
+    // Parse the title of the video
+    // TODO: Use Deezer's API or YouTube's metedata
+    var components = title.split('-');
+    var artist = components.shift();
+    if (components[components.length - 1] == " YouTube") {
+        components.pop()
+    }
+    var songTitle = components.join('|');
+
+    renderStatus('Performing Lyrics.ovh search for artist:' + artist
+                + ', and song name:' + songTitle);
+
+    getLyrics(artist, songTitle, function(lyrics) {
+      renderStatus(lyrics);
+    }, function(errorMessage) {
+      renderStatus('Cannot display lyrics. ' + errorMessage);
+    });
+  });
+});
